1d26d712cf67a67aa60be48ec1765706eb30a397
Viacheslav <vaceslavkozin619@gmail.com>
2022-08-12 15:57:26 +0200

    Running multiple processes

--- elshell.el
+++ elshell.el
@@ -1,7 +1,17 @@
-(defmacro escript (l)
-  (start-process-shell-command "no" nil
-                               (mapconcat 'symbol-name l " ")))
+(defmacro escript (&rest escripts)
+  (progn
+    (start-process-shell-command
+     "no"
+     (get-buffer-create "*escript-3*")
+     (mapconcat
+      (lambda (command)
+        (mapconcat 'symbol-name command " ")) escripts " ; "))
+    (sleep-for 3)
+    (with-current-buffer "*escript-3*"
+      (buffer-string))))
 
-(escript (killall pnmixer))
+(escript
+ (ls)
+ (pwd))
 
 (provide 'elshell)
