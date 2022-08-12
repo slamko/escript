95916452d660bb9cf0e51057a5232eaab7b0c4ea
Viacheslav <vaceslavkozin619@gmail.com>
2022-08-12 19:59:07 +0200

    Allow string as list arguments

--- elshell.el
+++ elshell.el
@@ -1,17 +1,43 @@
 (defmacro escript (&rest escripts)
   (progn
     (start-process-shell-command
-     "no"
-     (get-buffer-create "*escript-3*")
+     "escript"
+	 (get-buffer "*Messages*")
      (mapconcat
       (lambda (command)
-        (mapconcat 'symbol-name command " ")) escripts " ; "))
-    (sleep-for 3)
-    (with-current-buffer "*escript-3*"
-      (buffer-string))))
+        (mapconcat
+		 (lambda (cmd-sym)
+		   (cond ((symbolp cmd-sym) (symbol-name cmd-sym))
+				 ((stringp cmd-sym) cmd-sym))) command " ")) escripts " ; "))))
+
+(defmacro escript-output (&rest escripts)
+  (progn
+	(let* ((reverse-scripts (reverse escripts))
+		  (no-out-commands (reverse (cdr reverse-scripts)))
+		  (last-out-command (car reverse-scripts))
+		  (out-buf-name (concat "escript-"(number-to-string (random)))))
+	  
+      (start-process-shell-command
+       "escript" nil (mapconcat
+					  (lambda (command)
+						(mapconcat (lambda (cmd-sym)
+		   (cond ((symbolp cmd-sym) (symbol-name cmd-sym))
+				 ((stringp cmd-sym) cmd-sym))) command " ")) no-out-commands  " ; "))
+	  
+	  (start-process-shell-command
+       out-buf-name
+       (get-buffer-create out-buf-name)
+       (mapconcat 'symbol-name last-out-command " "))
+
+	  (sleep-for 1)
+	  (with-current-buffer out-buf-name
+		(cdr (cdr (cdr (reverse (split-string (buffer-string) "\n")))))))))
 
 (escript
- (ls)
- (pwd))
+ (ls "/"))
+ 
+(escript
+ (escript-output
+  (ls "/bin/pwd")))
 
 (provide 'elshell)
