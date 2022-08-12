72b21f222c4a2bb2a05f02ac0a7d97dacd2ba734
Viacheslav <vaceslavkozin619@gmail.com>
2022-08-13 01:39:24 +0200

    F*ck out macroses

--- elshell.el
+++ elshell.el
@@ -1,16 +1,28 @@
-(defmacro escript (&rest escripts)
+(defun escript-all (&rest escripts)
   (progn
     (start-process-shell-command
      "escript"
-	 (get-buffer "*Messages*")
+	 (get-buffer-create "*somewfwe*")
      (mapconcat
       (lambda (command)
         (mapconcat
 		 (lambda (cmd-sym)
 		   (cond ((symbolp cmd-sym) (symbol-name cmd-sym))
-				 ((stringp cmd-sym) cmd-sym))) command " ")) escripts " ; "))))
+				 ((stringp cmd-sym) cmd-sym))) command " ")) escripts ";"))))
 
-(defmacro escript-output (&rest escripts)
+(defun escript-and (&rest escripts)
+  (progn
+    (start-process-shell-command
+     "escript"
+	 nil
+     (mapconcat
+      (lambda (command)
+        (mapconcat
+		 (lambda (cmd-sym)
+		   (cond ((symbolp cmd-sym) (symbol-name cmd-sym))
+				 ((stringp cmd-sym) cmd-sym))) command " ")) escripts "&&"))))
+
+(defun escript-output (&rest escripts)
   (progn
 	(let* ((reverse-scripts (reverse escripts))
 		  (no-out-commands (reverse (cdr reverse-scripts)))
@@ -18,26 +30,34 @@
 		  (out-buf-name (concat "escript-"(number-to-string (random)))))
 	  
       (start-process-shell-command
-       "escript" nil (mapconcat
-					  (lambda (command)
-						(mapconcat (lambda (cmd-sym)
-		   (cond ((symbolp cmd-sym) (symbol-name cmd-sym))
-				 ((stringp cmd-sym) cmd-sym))) command " ")) no-out-commands  " ; "))
+       "escript" nil
+	   (mapconcat
+		(lambda (command)
+		  (mapconcat
+		   (lambda (cmd-sym)
+			 (cond ((symbolp cmd-sym) (symbol-name cmd-sym))
+				   ((stringp cmd-sym) cmd-sym))) command " ")) no-out-commands  ";"))
 	  
-	  (start-process-shell-command
-       out-buf-name
-       (get-buffer-create out-buf-name)
-       (mapconcat 'symbol-name last-out-command " "))
+	  (let ((escript-proc (start-process-shell-command
+						   out-buf-name
+						   (get-buffer-create out-buf-name)
+						   (mapconcat
+							(lambda (cmd-sym)
+							  (cond ((symbolp cmd-sym) (symbol-name cmd-sym))
+									((stringp cmd-sym) cmd-sym))) last-out-command " "))))
+		
+		(while (accept-process-output escript-proc)))
 
-	  (sleep-for 1)
 	  (with-current-buffer out-buf-name
 		(cdr (cdr (cdr (reverse (split-string (buffer-string) "\n")))))))))
 
-(escript
- (ls "/"))
- 
-(escript
- (escript-output
+(escript-all
+ (escript-output '(ls "/bin/pwd")))
+
+(defmacro print-escript (str)
+  (print (car (eval str))))
+
+(setq some (escript-output
   (ls "/bin/pwd")))
 
 (provide 'elshell)
