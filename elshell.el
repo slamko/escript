8df3c11913403b7efc904720387382937a933109
Viacheslav <vaceslavkozin619@gmail.com>
2022-08-13 13:05:39 +0200

    Generating variables with system binary names for shell autocomplete

--- elshell.el
+++ elshell.el
@@ -51,13 +51,43 @@
 	  (with-current-buffer out-buf-name
 		(cdr (cdr (cdr (reverse (split-string (buffer-string) "\n")))))))))
 
+
+(defun escript--get-bin-directories ()
+  (let ((path-var (getenv "PATH")))
+	(when path-var
+	  (split-string path-var ":"))))
+
+(defun escript--all-bins (bin-directories)
+  (let (all-bin-names)
+	(dolist (bin-path bin-directories all-bin-names)
+	  (if (file-exists-p bin-path)
+		  (setq all-bin-names (append (directory-files bin-path t "^[a-zA-Z0-9].*$") all-bin-names))))))
+
+(defun escript--unique-bins (bins)
+  (let (unique-bin-names)
+	(dolist (bin-path bins unique-bin-names)
+	  (let ((bin-symbol (intern (file-name-nondirectory bin-path))))
+		(if (and
+             (not (assq bin-symbol unique-bin-names))
+             (file-executable-p bin-path) (not (file-directory-p bin-path)))
+			(setq unique-bin-names (push (cons bin-symbol bin-path) unique-bin-names)))))))
+
+(defun escript--list-binaries (&optional bin-path)
+  (escript--unique-bins
+   (escript--all-bins
+	(if bin-path (list bin-path) (escript--get-bin-directories)))))
+
+(defun escript--setq-bin-name (bin-name-cell)
+  (set (car bin-name-cell) (cdr bin-name-cell)))
+
+(defun escript--define-bin-vars (binary-cells)
+  (mapc 'escript--setq-bin-name  binary-cells))
+
+(escript--define-bin-vars (escript--list-binaries))
+
 (escript-all
- (escript-output '(ls "/bin/pwd")))
+ (escript-output '(acceleration_speed))
+ '(emacs-28.1)
+ '(MagickCore-config))
 
-(defmacro print-escript (str)
-  (print (car (eval str))))
-
-(setq some (escript-output
-  (ls "/bin/pwd")))
-
-(provide 'elshell)
+(provide 'escript)
